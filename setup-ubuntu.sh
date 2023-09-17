922b7c0e412a6ed817d88a96aaaeb59a4f49e76d
Jerome Cabugwason <jeromecabugwason2@protonmail.com>
2023-09-17 18:40:45 +0800

    fix node depricated script

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -21,8 +21,13 @@ sudo apt-get install -y default-jdk maven
 echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64" >> ~/.bashrc
 
 # Install Node.js
-curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=18
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install nodejs -y
 
 # Install Android SDK
 cd ~/
